53e70835a67eecce2ed0bdcfb5fa167d6c932255
NhanNguyen000 <nhan.nguyenthi1412@gmail.com>
2023-06-28 14:50:05 +0200

    feat: boxplot for protein and metaboltie overtime (with statistic)

--- scripts/09_runPlink_02_gwas_03_linkSNPgenes_toProteins_v1.R
+++ scripts/09_runPlink_02_gwas_03_linkSNPgenes_toProteins_v1.R
@@ -7,8 +7,8 @@ strains <- c("H1N1", "H3N2", "B")
 sig_SNIPs <- list()
 for (strain in strains) {
   sig_SNIPs[[strain]] <- read.table(
-    file = paste0("/vol/projects/CIIM/Influenza/iMED/genotype/nhan_ll_vs_other_gwas/qtl_t4_abtiters/", 
-                  tolower(strain), "_suggestivesig.txt"), header = TRUE)
+    file = paste0("/vol/projects/CIIM/Influenza/iMED/genotype/nhan_ll_vs_other_gwas/new/suggestivesig_", 
+                  strain, ".txt"), header = TRUE)
 }
 
 loci <- sig_SNIPs %>% 
@@ -16,7 +16,7 @@ loci <- sig_SNIPs %>%
   dplyr::rename('rsid' = SNP, 'pval'= p)
 
 ## info for sig. SNPs --------------------------------------
-load("loci_info_qtl.RData")
+load("loci_info_gwas.RData")
 
 identical(loci_info$gwas$snps,loci_info$eqtl$snps) # TRUE, use one of them
 loci_strains <- loci %>% full_join(loci_info$gwas$snps)
