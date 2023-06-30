3b3635d0abfb7fa8bfcec908b200f51b2fd821b2
NhanNguyen000 <nhan.nguyenthi1412@gmail.com>
2023-06-30 15:25:05 +0200

    feat: SNPs distribution with the H3N2 T4 antibody titer

---
+++ scripts/09_runPlink_03_qtl_04_sigSNP_dist.R
@@ -0,0 +1,36 @@
+rm(list = ls())
+library(tidyverse)
+
+# load data =================================================
+load("cohorts_dat.RData")
+sigSNPs <- read.table("sendOut/qtl_sigSNPs_H3N2.txt", header = TRUE)
+sigSNPs_genotype_raw <- read.table("processedDat/qtl_sigSNPs_H3N2_genotype.txt", header = TRUE)
+
+sigSNPs_genotype <- sigSNPs_genotype_raw %>% 
+  mutate(across(c(10:168), function(x) substring(x, 1, 3))) %>%
+  mutate(across(c(10:168), 
+                function(x) paste0(ifelse(substring(x, 1, 1) == "0", REF, 
+                                          ifelse(substring(x, 1, 1) == "1", ALT, 
+                                                 substring(x, 1, 1))),
+                                   ifelse(substring(x, 3, 3) == "0", REF, 
+                                          ifelse(substring(x, 3, 3) == "1", ALT, 
+                                                 substring(x, 1, 1)))))) %>%
+  separate(col = ID, into = c("CHR:POS", "rsid"), sep = "%") %>% 
+  select(rsid, matches("I.")) %>% select(-2, -3) %>%
+  column_to_rownames("rsid") %>% 
+  t() %>% as.data.frame() %>% 
+  rownames_to_column("probandID") %>% mutate(probandID = gsub("[.]", "-", probandID)) 
+
+sigSNPs_genotype_H3N2_T4 <- sigSNPs_genotype %>% left_join(cohorts$HAI_all)
+
+plotList <- list()
+for (rsid in sigSNPs$rsid) {
+  plotList[[rsid]] <- sigSNPs_genotype_H3N2_T4 %>% select(c("probandID", rsid, "H3N2_T4")) %>% 
+    ggplot(aes_string(x = rsid, y = "H3N2_T4")) + 
+    geom_boxplot() + geom_jitter(position=position_jitter(0.2)) + 
+    theme_bw()
+}
+
+pdf(file = "sendOut/sigSNPs_dist_H3N2_T4.pdf", width = 18, height = 12)
+gridExtra::grid.arrange(grobs = plotList, ncol = 6)
+dev.off()
