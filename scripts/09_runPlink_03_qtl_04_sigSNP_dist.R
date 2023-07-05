408d1280ead0af6e18c380cc3c2d3b9f21650e88
NhanNguyen000 <nhan.nguyenthi1412@gmail.com>
2023-07-05 17:29:55 +0200

    feat: check the SNPs from the OmicsPrd model and plot

--- scripts/09_runPlink_03_qtl_04_sigSNP_dist.R
+++ scripts/09_runPlink_03_qtl_04_sigSNP_dist.R
@@ -6,6 +6,7 @@ load("cohorts_dat.RData")
 sigSNPs <- read.table("sendOut/qtl_sigSNPs_H3N2.txt", header = TRUE)
 sigSNPs_genotype_raw <- read.table("processedDat/qtl_sigSNPs_H3N2_genotype.txt", header = TRUE)
 
+# convert genotype keep 2 hetero-types -----------------------
 sigSNPs_genotype <- sigSNPs_genotype_raw %>% 
   mutate(across(c(10:168), function(x) substring(x, 1, 3))) %>%
   mutate(across(c(10:168), 
@@ -21,6 +22,7 @@ sigSNPs_genotype <- sigSNPs_genotype_raw %>%
   t() %>% as.data.frame() %>% 
   rownames_to_column("probandID") %>% mutate(probandID = gsub("[.]", "-", probandID)) 
 
+# convert genotype merge 2 hetero-types into one -----------------------
 sigSNPs_genotype_v2 <- sigSNPs_genotype_raw %>% 
   mutate(across(c(10:168), function(x) paste0(substring(x, 1, 1), substring(x, 3, 3)))) %>%
   mutate(across(c(10:168), 
@@ -34,10 +36,10 @@ sigSNPs_genotype_v2 <- sigSNPs_genotype_raw %>%
   t() %>% as.data.frame() %>% 
   rownames_to_column("probandID") %>% mutate(probandID = gsub("[.]", "-", probandID)) 
 
-
-#sigSNPs_genotype_H3N2_T4 <- sigSNPs_genotype %>% left_join(cohorts$HAI_all)
+# genotype + abTiter data, and plot  -----------------------
+#sigSNPs_genotype_H3N2_T4 <- sigSNPs_genotype %>% left_join(cohorts$HAI_all) # use raw abTiter T4
 sigSNPs_genotype_H3N2_T4 <- sigSNPs_genotype_v2 %>% 
-  left_join(cohorts$HAI_all) %>% mutate(H3N2_T4_log2 = log2(H3N2_T4))
+  left_join(cohorts$HAI_all) %>% mutate(H3N2_T4_log2 = log2(H3N2_T4)) # use log2(abTiter T4)
 
 plotList <- list()
 for (rsid in sigSNPs$rsid) {
@@ -49,5 +51,30 @@ for (rsid in sigSNPs$rsid) {
 }
 
 pdf(file = "sendOut/sigSNPs_dist_H3N2_T4.pdf", width = 18, height = 12)
+gridExtra::grid.arrange(grobs = plotList, ncol = 6)
+dev.off()
+
+# use normalized(abTiter T4) - Martijn did ----------------
+normalized_abTiter <- read.table("/vol/projects/CIIM/Influenza/iMED/genotype/nhan_ll_vs_other_gwas/qtl_t4_abtiters/phenotypes.csv") %>% 
+  t() %>% as.data.frame %>% rownames_to_column("probandID") %>%
+  mutate(probandID = gsub("X", "", probandID)) %>%
+  mutate(probandID = ifelse(nchar(probandID) == 1, 
+                            paste0("I-000", probandID), 
+                            ifelse(nchar(probandID) == 2, paste0("I-00", probandID), 
+                                   ifelse(nchar(probandID) == 3, 
+                                          paste0("I-0", probandID), paste0("I-", probandID)))))
+
+sigSNPs_genotype_H3N2_T4 <- sigSNPs_genotype_v2 %>% 
+  left_join(normalized_abTiter)
+
+plotList <- list()
+for (rsid in sigSNPs$rsid) {
+  plotList[[rsid]] <- sigSNPs_genotype_H3N2_T4 %>% select(c("probandID", rsid, "H3N2_T4")) %>% 
+    ggplot(aes_string(x = rsid, y = "H3N2_T4")) + 
+    geom_boxplot() + geom_jitter(position=position_jitter(0.2)) + 
+    theme_bw()
+}
+
+pdf(file = "sendOut/sigSNPs_normalizedDist_H3N2_T4.pdf", width = 18, height = 12)
 gridExtra::grid.arrange(grobs = plotList, ncol = 6)
 dev.off()
---
+++ scripts/09_runPlink_04_omicspred_01_protein_03_existProModels.R
@@ -0,0 +1,106 @@
+rm(list = ls())
+
+library(tidyverse)
+library(ggpubr)
+# load data =======================================================================
+proPred_dat <- read.csv2("data/Olink_trait_validation_results_with_OMICSPRED_ID.csv", sep = "\t")
+load("selected_DAPs.RData")
+
+selected_proPred <- proPred_dat %>% 
+  filter(Gene %in% selected_DAs) %>% 
+  select(OMICSPRED.ID, Gene, X.SNP, Internal_R2, NSPHS_R2, ORCADES_R2)
+
+# extract file ----------------------------------------------------------
+model_files <- list.files("processedDat/omicsPred/Olink_output")[
+  which(substring(list.files("processedDat/omicsPred/Olink_output"), 1, 10) 
+        %in% selected_proPred$OMICSPRED.ID)]
+
+model_info <- list()
+model_outcome <- list()
+model_rsid <- list()
+for (model_id in unique(substring(model_files, 1, 10))) {
+  model_info[[model_id]] <- read.delim(paste0("processedDat/omicsPred/Olink_output/", 
+                                             model_id, "_model.txt.log"), header = FALSE)
+  model_outcome[[model_id]] <- read.delim(paste0("processedDat/omicsPred/Olink_output/", 
+                                                model_id, "_model.txt.sscore"))
+  model_rsid[[model_id]] <- read.delim(paste0("processedDat/omicsPred/Olink_output/", 
+                                             model_id, "_model.txt.sscore.vars"), header = FALSE)
+}
+
+# get model running information -----------------------------
+model_info_v2 <- model_info %>% 
+  lapply(function(x) if(nrow(x) == 24) {x %>% add_row(V1 = NA, .before = 19)} else x) %>%
+  imap(~.x %>% rename_with(function(x) .y)) %>% 
+  purrr::reduce(cbind)
+
+pred_model_info <- model_info_v2[19:20, ] %>% t() %>% as.data.frame() %>% 
+  rename("warning" = "19", "number_SNPs_use" = "20") %>%
+  mutate(number_SNPs_use = gsub("--score: | variant processed.| variants processed.",
+                                "", number_SNPs_use)) %>% 
+  rownames_to_column("OMICSPRED.ID") %>% 
+  full_join(selected_proPred %>% rename("number_SNPs_total" = "X.SNP")) %>% 
+  relocate(number_SNPs_use, .after = "number_SNPs_total")
+
+# get model outcome --------------------------------------
+predicted_dat <- model_outcome %>% 
+  lapply(function(x) x %>% as.data.frame %>% column_to_rownames("X.IID")) %>%
+  imap(~.x %>% rename_with(function(x) .y)) %>% 
+  purrr::reduce(cbind) %>% 
+  t() %>% as.data.frame %>% 
+  rownames_to_column("OMICSPRED.ID") %>% full_join(pred_model_info %>% select(OMICSPRED.ID, Gene)) %>%
+  column_to_rownames("Gene") %>% select(-OMICSPRED.ID) %>% t() %>% as.data.frame()
+
+# check the correlation between prediction and real data -------------------
+load("cohorts_dat.RData")
+
+## protein measures from coresspoding subjects
+measured_dat <- cohorts$donorSample_all %>% 
+  filter(time == "T1") %>% select(probandID, name) %>%
+  filter(probandID %in% rownames(predicted_dat)) %>%
+  left_join(protein_Dat$iMED_2015 %>% rownames_to_column("name")) %>%
+  arrange(match(probandID, rownames(predicted_dat))) %>% 
+  column_to_rownames("probandID") %>% select(-name)
+
+identical(rownames(predicted_dat), rownames(measured_dat)) # TRUE = same order of subjects
+
+# calculate the r and p value
+for (protein_name in pred_model_info$Gene) {
+  out_temp <- cor.test(predicted_dat[, protein_name], measured_dat[, protein_name], 
+                       use = "complete.obs")
+  pred_model_info$cor[pred_model_info$Gene == protein_name] <- out_temp$estimate
+  pred_model_info$pval[pred_model_info$Gene == protein_name] <- out_temp$p.value
+}
+
+# make scatter plot
+protein <- "CXCL1"
+protein <- "IL10RB"
+protein <- "OSCAR"
+protein <- "IL15RA"
+protein <- "LTA"
+protein <- "TNFRSF11A"
+protein <- "FGF5"
+protein <- "CD4"
+protein <- "TNFSF10"
+protein <- "VEGFA"
+protein <- "LIFR"
+protein <- "CCL7"
+protein <- "CXCL8"
+protein <- "OSM"
+protein <- "IL6"
+protein <- "SIGLEC1"
+
+dat_temp <- cbind("predicted" = predicted_dat[, protein],
+                  "measured" = measured_dat[, protein])
+
+dat_temp %>% as.data.frame() %>% 
+  ggplot(aes(x = measured, y = predicted)) + 
+  geom_point() + 
+  stat_smooth(method = "lm", col = "blue") + stat_cor() + 
+  theme_classic() + ggtitle(protein)
+
+# check rsid ------------------
+protein <- "CXCL1"
+write.table(model_rsid[[pred_model_info$OMICSPRED.ID[pred_model_info$Gene == protein]]],
+            file = paste0("processedDat/omicsPred/", protein, "_rsid.txt"),
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
+
