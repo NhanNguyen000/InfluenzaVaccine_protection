3ed17cefae8c5c240bbf3c0324ba0f5e1bc5fef9
NhanNguyen000 <nhan.nguyenthi1412@gmail.com>
2023-10-24 17:32:04 +0200

    feat: run sbatch for prediction models

---
+++ scripts/20_run_sbatch_05_predModels_01_rf_tuneLength.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=predModel_rf       # Name of job
+#SBATCH --output=scripts/predModel_rf.out        # stdout
+#SBATCH --error=scripts/predModel_rf.err         # stderr
+#SBATCH --partition=cpu           # partition to use (check with sinfo)
+#SBATCH --nodes=1                 # Number of nodes
+#SBATCH --ntasks=1                # Number of tasks | Alternative: --ntasks-per-node
+#SBATCH --threads-per-core=1      # Ensure we only get one logical CPU per core
+#SBATCH --cpus-per-task=1         # Number of cores per task
+#SBATCH --mem=16G                 # Memory per node | Alternative: --mem-per-cpu
+#SBATCH --time=30:00:00            # wall time limit (HH:MM:SS)
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nhan.nguyen@helmholtz-hzi.de
+#SBATCH --clusters=bioinf
+
+cd /vol/projects/BIIM/Influenza/ZirrFlu/InfluenzaCohorts_NhanNguyen/
+Rscript scripts/16_predModels_01_rf_tuneLength.R
+
+
+/usr/bin/hostname
+
---
+++ scripts/20_run_sbatch_05_predModels_06_xgboost.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=predModel_xgboost       # Name of job
+#SBATCH --output=scripts/predModel_xgboost.out        # stdout
+#SBATCH --error=scripts/predModel_xgboost.err         # stderr
+#SBATCH --partition=cpu           # partition to use (check with sinfo)
+#SBATCH --nodes=1                 # Number of nodes
+#SBATCH --ntasks=1                # Number of tasks | Alternative: --ntasks-per-node
+#SBATCH --threads-per-core=1      # Ensure we only get one logical CPU per core
+#SBATCH --cpus-per-task=1         # Number of cores per task
+#SBATCH --mem=16G                 # Memory per node | Alternative: --mem-per-cpu
+#SBATCH --time=30:00:00            # wall time limit (HH:MM:SS)
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nhan.nguyen@helmholtz-hzi.de
+#SBATCH --clusters=bioinf
+
+cd /vol/projects/BIIM/Influenza/ZirrFlu/InfluenzaCohorts_NhanNguyen/
+Rscript scripts/16_predModels_06_xgboost.R
+
+
+/usr/bin/hostname
+
